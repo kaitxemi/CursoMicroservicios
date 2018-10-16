fde9eb7dd4563de17ccc0b952090e4b613bbc01f
kaitxemi <kaitxemi@gmail.com>
2018-10-16 17:17:49 +0200

    practica 9 subida

--- EurekaNamingService/src/main/java/com/practicas/EurekaNamingServiceApplication.java
+++ EurekaNamingService/src/main/java/com/practicas/EurekaNamingServiceApplication.java
@@ -3,6 +3,9 @@ package com.practicas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+
+import brave.sampler.Sampler;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -11,4 +14,9 @@ public class EurekaNamingServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaNamingServiceApplication.class, args);
 	}
+	 @Bean
+	    public Sampler defaultSampler(){
+	        return Sampler.ALWAYS_SAMPLE;
+	    }
+
 }
--- limits-service/src/main/java/com/practicas/Practica1Application.java
+++ limits-service/src/main/java/com/practicas/Practica1Application.java
@@ -3,6 +3,9 @@ package com.practicas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+import brave.sampler.Sampler;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -11,4 +14,9 @@ public class Practica1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Practica1Application.class, args);
 	}
+	 @Bean
+	    public Sampler defaultSampler(){
+	        return Sampler.ALWAYS_SAMPLE;
+	    }
+
 }
