b3dd3e0749ca987d3e5361b0081a6d93b7906718
kaitxemi <kaitxemi@gmail.com>
2018-10-17 16:14:32 +0200

    subir oauth2

---
+++ Oauth2AutorizationService/src/main/java/com/practicas/Oauth2AutorizationServiceApplication.java
@@ -0,0 +1,12 @@
+package com.practicas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2AutorizationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2AutorizationServiceApplication.class, args);
+	}
+}
